f5f6c85ef0b34b2925030514e6284b56133e2a33
alexbrown <ajb2969@rit.edu>
2019-10-13 19:06:30 -0400

    modified functionality to create docker machines. Currently working through dataflow and datalabeling with normal and attack.sh

--- Environment/create-docker.sh
+++ Environment/create-docker.sh
@@ -5,5 +5,20 @@ echo "======> Creating $workers worker machines ...";
 for node in $(seq 1 $workers);
 do
 	echo "======> Creating worker$node machine ...";
-	docker-machine create --driver virtualbox default$node;
+	docker create -it --net host --name worker$node alpine /bin/sh;
 done
+
+echo "======> Starting $workers worker machines ...";
+for node in $(seq 1 $workers);
+do
+	echo "======> Starting worker$node machine ...";
+	docker container start worker$node;
+done
+
+
+#echo "======> Removing $workers worker machines ...";
+#for node in $(seq 1 $workers);
+#do
+#	echo "======> Starting worker$node machine ...";
+#	docker container rm worker$node;
+#done
