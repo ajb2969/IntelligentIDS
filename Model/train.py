eacb131d5c6ba5c5024947e574647bdb9c1f7a79
alexbrown <ajb2969@rit.edu>
2019-10-29 11:28:47 -0400

    changed parameters to model

--- Model/train.py
+++ Model/train.py
@@ -2,8 +2,7 @@ from __future__ import print_function
 import numpy as np
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import Dense, Embedding
-from keras.layers import LSTM
+from keras.layers import Dense, Embedding, LSTM
 
 x_train_file = "../Data/x_train.csv"
 x_test_file = "../Data/x_test.csv"
@@ -20,35 +19,31 @@ def import_data():
 
 
 max_features = 20000
-# cut texts after this number of words (among top max_features most common words)
-maxlen = 80
-batch_size = 32
+maxlen = 12000
+batch_size = 64
 print('Loading data...')
 (x_train, y_train), (x_test, y_test) = import_data()
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
 
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
+#x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+#x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+#print('x_train shape:', x_train.shape)
+#print('x_test shape:', x_test.shape)
 
-print('Build model...')
+print('Building model')
 model = Sequential()
-model.add(Embedding(max_features, 128))
-model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Embedding(max_features, 256))
+model.add(LSTM(256, dropout=0.1, recurrent_dropout=0.1))
 model.add(Dense(1, activation='sigmoid'))
 
-# try using different optimizers and different optimizer configs
+#binary classification, adam optimizer worked the best
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-print('Train...')
+print('Training model')
 model.fit(x_train, y_train,
           batch_size=batch_size,
-          epochs=15,
+          epochs=5,
           validation_data=(x_test, y_test))
 score, acc = model.evaluate(x_test, y_test,
                             batch_size=batch_size)
